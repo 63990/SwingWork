3dbe6d5b632b36bcc79c398723e6b24ee86942e8
63990 <eriksesr@ntlworld.com>
2018-02-07 10:50:11 +0000

    Plotting Square Wave

--- Wrapper.m
+++ Wrapper.m
@@ -12,10 +12,18 @@ theta0 = pi/32;
 
 % Solve t and v 
 [t, v] = ode45(@VarPendulum, [T_start T_end], [theta_dot0 theta0]);
+theta = v(:,2);
+theta_dot = v(:,1);
 
+r = zeros(length(theta));
+for i = 1:length(theta)
+[~, r(i)] = body(theta_dot(i),theta(i));
+end
 % Plotting
 figure;
+hold on;
 plot(t, wrapToPi(v(:,2)));
+plot(t, r-0.9);
 title('Oscillation Change');
 xlabel('time (s)');
 ylabel('theta (rad)');
