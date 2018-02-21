038eff8b4937d466ddd30330df2e705007c84866
63990 <eriksesr@ntlworld.com>
2018-02-21 10:36:47 +0000

    Working r now
    signs changed around

--- NewModel/pendulum.m
+++ NewModel/pendulum.m
@@ -14,7 +14,7 @@ theta_dot = v(2);
 [r, dr_dtheta, dr_dtheta_dot] = radius(theta, theta_dot);
 
 % Calculate the second time differential of theta
-theta_double_dot = (-g*sin(theta) - 2*(theta_dot^2)*dr_dtheta)/...
+theta_double_dot = -(g*sin(theta) + 2*(theta_dot^2)*dr_dtheta)/...
                          (r + 2*theta_dot*dr_dtheta_dot);
 
 v_dot = [theta_dot, theta_double_dot]';
--- NewModel/radius.m
+++ NewModel/radius.m
@@ -14,41 +14,41 @@ if (theta_dot > 0) && (abs(theta) < theta_lim)
     % As theta approaches 0 from the negative theta direction (theta_dot >
     % 0), the swinger gets to theta = theta_lim. At this point the swinger
     % begins to stand.
-    r = r_av + multiplier*h*tanh(m*theta);
-    dr_dtheta = (multiplier*h*m)/(cosh(m*theta)^2);
+    r = r_av - multiplier*h*tanh(m*theta);
+    dr_dtheta = -(multiplier*h*m)/(cosh(m*theta)^2);
     dr_dtheta_dot = 0;
 elseif (theta_dot < 0) && (abs(theta) < theta_lim)
     % As theta approaches 0 from the positive theta direction (theta_dot <
     % 0), the swinger gets to theta = theta_lim. At this point the swinger
     % begins to stand.
-    r = r_av + multiplier*h*tanh(-m*theta);
-    dr_dtheta = (-multiplier*h*m)/(cosh(-m*theta)^2);
+    r = r_av - multiplier*h*tanh(-m*theta);
+    dr_dtheta = (multiplier*h*m)/(cosh(-m*theta)^2);
     dr_dtheta_dot = 0;
 elseif (theta > 0) && (abs(theta_dot) < theta_dot_lim)
     % As theta increases positively, the swinger goes up to maximum of the swing.
     % Theta_dot decreases as theta goes the the maximum until theta_dot ==
     % theta_dot_lim. At this point the person sits. 
-    r = r_av + multiplier*h*tanh(n*theta_dot);
+    r = r_av - multiplier*h*tanh(n*theta_dot);
     dr_dtheta = 0;
-    dr_dtheta_dot = (multiplier*h*n)/(cosh(n*theta_dot)^2);
+    dr_dtheta_dot = -(multiplier*h*n)/(cosh(n*theta_dot)^2);
 elseif (theta < 0) && (abs(theta_dot) < theta_dot_lim)
     % As theta increases negatively, the swinger goes up to maximum of the swing.
     % Theta_dot decreases as theta goes the the maximum until theta_dot ==
     % theta_dot_lim. At this point the person sits. 
-    r = r_av + multiplier*h*tanh(-n*theta_dot);
+    r = r_av - multiplier*h*tanh(-n*theta_dot);
     dr_dtheta = 0;
-    dr_dtheta_dot = (-multiplier*h*n)/(cosh(-n*theta_dot)^2);
+    dr_dtheta_dot = (multiplier*h*n)/(cosh(-n*theta_dot)^2);
 elseif (theta*theta_dot) > 0
     % If theta and theta_dot are both the same sign then the swinger is 
     % accelerating towards the maximum of the swing. In this case they should
     % be standing.
-    r = r_av + h;
+    r = r_av - h;
     dr_dtheta = 0;
     dr_dtheta_dot = 0;
 else
     % If theta and theta_dot are different signs then the swinger is 
     % accelerating towards theta = 0. In this case they should be sitting.
-    r = r_av - h;
+    r = r_av + h;
     dr_dtheta = 0;
     dr_dtheta_dot = 0;
 end
